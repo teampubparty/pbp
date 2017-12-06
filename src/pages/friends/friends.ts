f8adec5009b9190ba4e9f874e55411f90501b15f
Adiontae Gerron <pubparty.team@gmail.com>
2017-12-06 09:03:18 -0600

    asfdfasf

--- src/pages/friends/friends.ts
+++ src/pages/friends/friends.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ModalController, ActionSheetController } from 'ionic-angular';
 import * as firebase from 'firebase';
 import 'firebase/firestore';
+import { ToastController } from 'ionic-angular/components/toast/toast-controller';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @IonicPage()
 @Component({
@@ -12,6 +14,8 @@ import 'firebase/firestore';
 export class FriendsPage {
 
   constructor(
+    public alertCtrl: AlertController,
+    public toastCtrl: ToastController,
     public navCtrl: NavController, 
     public navParams: NavParams,
     public modalCtrl: ModalController,
@@ -96,16 +100,145 @@ newFriend:any;
     this.modalCtrl.create("AddFriendPage").present()
   }
 
-  showFriendOptions(){
+  showFriendOptions(user){
     this.actionSheet.create({
       buttons: [
         {
-          text: "Unfriend"
+          text: "Unfriend",
+          handler: () => {
+            this.confirmUnfriend(user)
+          }
         }
       ]
     }).present()
   }
+  confirmUnfriend(user){
+    this.alertCtrl.create({
+      title: "Unfriend",
+      message: "Are you sure you want to unfriend this friend?",
+      buttons: [{
+        text: "Cancel"
+        },{
+          text: "Unfriend",
+          handler: data =>{
+            this.deletefriendCurrentUser(user, "Friend has been unfriended");
+            this.deletefriendOtherUser(user);
+          }
+        }] 
+      }).present()
+      }
+  confirmAcceptRequest(user){
+    this.alertCtrl.create({
+      title: "Accept Friend Request",
+      message: "Are you sure you want to accept this friend request?",
+      buttons: [{
+        text: "Cancel"
+        },{
+          text: "Accept",
+          handler: data =>{
+            this.acceptfriendCurrentUser(user);
+            this.acceptfriendOtherUser(user);
+          }
+        }] 
+      }).present()
+      }
 
+
+      acceptfriendCurrentUser(user){
+        firebase.firestore().doc("users/" + firebase.auth().currentUser.uid + "/friends/" + user.uid)
+        .update({
+          status: "Friends"
+        })
+        .then((sucess)=>{
+          this.toastCtrl.create({
+            message: "Friend Request has been accepted",
+            duration: 3000,
+            position: "top"
+          }).present()
+        })
+        .catch((error)=>{
+          this.toastCtrl.create({
+            message: error.message,
+            duration: 3000,
+            position: "top",
+          }).present()
+        })
+      }
+    acceptfriendOtherUser(user){
+      firebase.firestore().doc("users/" + user.uid + "/friends/" + firebase.auth().currentUser.uid)
+      .update({
+        status: "Friends"
+      })
+      .catch((error)=>{
+        this.toastCtrl.create({
+          message: error.message,
+          duration: 3000,
+          position: "top",
+        }).present()
+      })
+    }
+
+
+  confirmDeleteRequest(user){
+    this.alertCtrl.create({
+      title: "Delete Friend Request",
+      message: "Are you sure you want to delete this friend request?",
+      buttons: [{
+        text: "Cancel"
+        },{
+          text: "Delete",
+          handler: data =>{
+            this.deletefriendCurrentUser(user, "Friend Request has been deleted");
+          }
+        }] 
+      }).present()
+      }
+
+      confirmCancelPendingRequest(user){
+  this.alertCtrl.create({
+    title: "Cancel Pending Request",
+    message: "Are you sure you want to cancel your friend request?",
+    buttons: [{
+      text: "Do not Cancel"
+      },{
+        text: "Cancel Request",
+        handler: data =>{
+          this.deletefriendCurrentUser(user, "Friend Request has been canceled");
+          this.deletefriendOtherUser(user);
+        }
+      }] 
+    }).present()
+    }
+
+  deletefriendCurrentUser(user, message){
+      firebase.firestore().doc("users/" + firebase.auth().currentUser.uid + "/friends/" + user.uid)
+      .delete()
+      .then((sucess)=>{
+        this.toastCtrl.create({
+          message: message,
+          duration: 3000,
+          position: "top"
+        }).present()
+      })
+      .catch((error)=>{
+        this.toastCtrl.create({
+          message: error.message,
+          duration: 3000,
+          position: "top",
+        }).present()
+      })
+    }
+  deletefriendOtherUser(user){
+    firebase.firestore().doc("users/" + user.uid + "/friends/" + firebase.auth().currentUser.uid)
+    .delete()
+    .catch((error)=>{
+      this.toastCtrl.create({
+        message: error.message,
+        duration: 3000,
+        position: "top",
+      }).present()
+    })
+  }
   viewProfile(){
     this.navCtrl.push("ProfilePage")
   }
