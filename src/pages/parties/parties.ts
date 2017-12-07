59482225fa4a202283d822503c11a60cea8589cb
Adiontae Gerron <adiontae.gerron@gmail.com>
2017-12-07 16:25:14 -0600

    dfasfsda

--- src/pages/parties/parties.ts
+++ src/pages/parties/parties.ts
@@ -23,34 +23,33 @@ export class PartiesPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad PartiesPage');
     this.getFriends();
-    
-    
   }
 
   showCreateParty(){
     this.modalCtrl.create("CreatePartyPage").present();
   }
   FriendsPartiesComponent = "FriendsPartiesComponent";
+
 getFriends(){
   let parties = [];
   let friends = [];
-  
-
+  this.friendsParties = [];
+console.log(this.friendsParties);
   // get friends
-  firebase.firestore().collection("users/" + firebase.auth().currentUser.uid + "/friends/")
-  
-  .onSnapshot((friendSnap)=>{
-    
-    friendSnap.forEach((friend)=>{
-      friends.push(friend.data());
+
+      // get parties
+  firebase.firestore().collection("/parties/")
+  .limit(3)
+  .get().then((partiesSnap)=>{
+    partiesSnap.forEach((party)=>{
+      parties.push(party.data());
     })
   })
 
-  // get parties
-  firebase.firestore().collection("/parties/")
-  .onSnapshot((partiesSnap)=>{
-    partiesSnap.forEach((party)=>{
-      parties.push(party.data());
+  firebase.firestore().collection("users/" + firebase.auth().currentUser.uid + "/friends/")
+  .get().then((friendSnap)=>{
+    friendSnap.forEach((friend)=>{
+      friends.push(friend.data());
     })
 
     friends.forEach((friend)=>{
@@ -61,11 +60,33 @@ getFriends(){
       })
     })
   
+
   })
-  
- console.log(friends);
-  console.log(this.friendsParties);
+
 }
 
-  
+doRefresh(refresher) {
+
+  setTimeout(() => {
+    this.getFriends()
+    refresher.complete();
+  }, 2000);
+}
+
+doInfinite(infiniteScroll) {
+  console.log('Begin async operation');
+
+  setTimeout(() => {
+    let oldLength;
+    let newLength = oldLength + 3;
+
+    for (let i = 0; i < 3; i++) {
+      this.items.push( this.items.length );
+    }
+
+    console.log('Async operation has ended');
+    infiniteScroll.complete();
+  }, 500);
+}
+
 }
